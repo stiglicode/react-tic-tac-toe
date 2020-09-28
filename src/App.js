a543777a5a0176073d8d2c004ecddb7a0dfd131b
Milan Stiglic <stiglic.231@gmail.com>
2020-09-28 09:55:42 +0200

    Commit Menu view

--- src/App.js
+++ src/App.js
@@ -5,18 +5,18 @@ import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-d
 import './style/css/main.css';
 
 // views
-import Menu from './view/Menu';
+// import Menu from './view/Menu';
 import Game from './view/Game';
 
 const App = () => (
     <Router>
         <Switch>
             <Route path="/" exact>
-                <Redirect to="/menu" />
+                <Redirect to="/game" exact/>
             </Route>
-            <Route path="/menu" exact>
+            {/* <Route path="/menu" exact>
                 <Menu />
-            </Route>
+            </Route> */}
             <Route path="/game">
                 <Game />
             </Route>

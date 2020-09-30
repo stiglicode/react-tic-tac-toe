83630241d1b8bc0b772c2924070179e1fcbf1669
Milan Stiglic <stiglic.231@gmail.com>
2020-09-30 10:45:38 +0200

    And root view for GAME

--- src/view/Game.js
+++ src/view/Game.js
@@ -1,8 +1,20 @@
 import React from 'react';
 
+// Header 
+import HeaderMenu from './../components/HeaderMenu';
+// PLayfield
+import TurnDisplay from './../components/TurnDisplay';
+import Field from './../components/Field';
+import Stats from './../components/Stats';
+
 const Game = () => (
-    <div>
-        I am a game
+    <div className="game-wrapper column">
+        <HeaderMenu />
+        <div className="playfield bs-padd">
+            <TurnDisplay />
+            <Field />
+            <Stats />
+        </div>
     </div>
 );
 export default Game;

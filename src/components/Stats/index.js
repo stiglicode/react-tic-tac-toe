ef9e7386cca1ae9df31e9ca02bdab023000f3625
Milan Stiglic <stiglic.231@gmail.com>
2020-10-04 16:18:47 +0200

    Add template for Stats

--- src/components/Stats/index.js
+++ src/components/Stats/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import './css/Stats.component.css'
+
+// component 
+import WinnerStats from './WinnerStats';
 
 const Stats = () => (
     <div className="sts-box">
-        I am Stats
+        <div className="column sts-container">
+            <div className="sts-container_title column">
+                <span className="sts-container_title-static">Statistics in</span>
+                <span className="sts-container_title-dyniamic b">5 Games</span>
+            </div>
+            <div className="sts-container_content bs-padd">
+                <WinnerStats />
+            </div>
+        </div>
     </div>
 );
 export default Stats;

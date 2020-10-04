b6fc82c32cd51ca72f66b7659d24a76682d85eab
Milan Stiglic <stiglic.231@gmail.com>
2020-10-04 16:03:13 +0200

    Add class switcher for TurnDisplay

--- src/view/Game.js
+++ src/view/Game.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 // Header 
 import HeaderMenu from './../components/HeaderMenu';
@@ -7,14 +7,30 @@ import TurnDisplay from './../components/TurnDisplay';
 import Field from './../components/Field';
 import Stats from './../components/Stats';
 
-const Game = () => (
+const Game = () => {
+    const [onTurn, setTurn] = useState(null);
+
+    const callTurn = (turn) => {
+        setTurn(turn);
+    }
+    const callClass = () => {
+        const reset = (onTurn === null) ? setTurn("X") : false;
+        if(onTurn === "X") {
+            return "player-X"
+        } else  if(onTurn === "O") {
+            return "player-O"
+        }
+        return reset;
+    }
+    return (
     <div className="game-wrapper column">
         <HeaderMenu />
-        <div className="playfield bs-padd">
+        <div className={`playfield bs-padd ${callClass()}`}>
             <TurnDisplay />
-            <Field />
+            <Field getTurn={callTurn} />
             <Stats />
         </div>
     </div>
-);
+    )
+};
 export default Game;
